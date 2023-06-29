c527ccff4a94edae39ff6a5172e03d22fdfffb9c
gabriel-cini <115150782+gabriel-cini@users.noreply.github.com>
2023-06-29 11:09:36 +0200

    ajout jour 1 runtrackJs

---
+++ runtrackJS/job01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma page</title>
+    <script type="text/javascript">
+        window.onload = function() {
+            alert("Hello JavaScript !");
+        };
+    </script>
+</head>
+<body>
+
+</body>
+</html>
